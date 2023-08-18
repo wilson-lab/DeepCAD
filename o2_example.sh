9d6538f319f2568e809d3ee7ca63fd1871a53068
Bates <ab714@compute-a-16-171.o2.rc.hms.harvard.edu>
2023-08-18 16:08:29 -0400

    corrected o2 sbatch job example

--- o2_example.sh
+++ o2_example.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-#SBATCH -c 4                               # Request cores
-#SBATCH -p gpu_quad                        # Use a quad GPU
-#SBATCH --gres=gpu:1                       # Number to use
-#SBATCH -t 2-00:00                         # Runtime in D-HH:MM format
-#SBATCH --mem=50GB                         # Memory total in MiB (for all cores)
-#SBATCH -o jobs/deepcad_%j.out               # File to which STDOUT will be written, including job ID (%j)
-#SBATCH -e jobs/deepcad_%j.err               # File to which STDERR will be written, including job ID (%j)
+#SBATCH -c 1                               # Use a CPU
+#SBATCH --partition gpu_quad               # Use a quad GPU
+#SBATCH --gres=gpu:rtx8000:1               # Number to use
+#SBATCH --time=2-00:00                     # Runtime in D-HH:MM format
+#SBATCH --mem=100GB                        # Memory total (for all cores)
+#SBATCH -o jobs/deepcad_%j.out             # File to which STDOUT will be written, including job ID (%j)
+#SBATCH -e jobs/deepcad_%j.err             # File to which STDERR will be written, including job ID (%j)
 
-module load gcc/6.2.0 cuda/10.2 miniconda3/4.10.3 python/3.6.0
+module load gcc/6.2.0 
+module load cuda/10.2 
+module load miniconda3/4.10.3 
+module load python/3.6.0
 
-/n/cluster/bin/job_gpu_monitor.sh &        # collect information about the actual GPU utilization
-
-conda init bash
-conda activate deepcad
-python DeepCAD_pytorch /script.py train
+cd DeepCAD_pytorch
+source activate deepcad
+python script.py train
 echo "deepcad train complete"
-python DeepCAD_pytorch /script.py test
+python script.py test
 echo "deepcad test complete"
