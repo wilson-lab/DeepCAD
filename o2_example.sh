18f3432ac2cd373e6a2e80bc4571f1711e09ded8
alexanderbates <alexander.shakeel.bates@gmail.com>
2023-08-18 00:16:50 -0400

    Update o2_example.sh

--- o2_example.sh
+++ o2_example.sh
@@ -4,16 +4,16 @@
 #SBATCH --gres=gpu:1                       # Number to use
 #SBATCH -t 2-00:00                         # Runtime in D-HH:MM format
 #SBATCH --mem=50GB                         # Memory total in MiB (for all cores)
-#SBATCH -o jobs/$USER_%j.out                 # File to which STDOUT will be written, including job ID (%j)
-#SBATCH -e jobs/$USER_%j.err                 # File to which STDERR will be written, including job ID (%j)
+#SBATCH -o jobs/$USER_%j.out               # File to which STDOUT will be written, including job ID (%j)
+#SBATCH -e jobs/$USER_%j.err               # File to which STDERR will be written, including job ID (%j)
 
 module load gcc/6.2.0 cuda/10.2 miniconda3/4.10.3 python/3.6.0
 
 /n/cluster/bin/job_gpu_monitor.sh &        # collect information about the actual GPU utilization
 
-srun -c 1 cd /home/ab714/DeepCAD/DeepCAD_pytorch/
+srun -c 1 conda init bash
 srun -c 1 conda activate deep-cad
-python script.py train
+python DeepCAD_pytorch /script.py train
 srun -c 1 echo "deepcad train complete"
-python script.py test
+python DeepCAD_pytorch /script.py test
 srun -c 1 echo "deepcad test complete"
