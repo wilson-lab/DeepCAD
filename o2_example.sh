b038227b4fdad20fa2650ab5a70a418df5389729
alexanderbates <alexander.shakeel.bates@gmail.com>
2023-08-18 00:11:57 -0400

    Update o2_example.sh

--- o2_example.sh
+++ o2_example.sh
@@ -4,8 +4,8 @@
 #SBATCH --gres=gpu:1                       # Number to use
 #SBATCH -t 2-00:00                         # Runtime in D-HH:MM format
 #SBATCH --mem=50GB                         # Memory total in MiB (for all cores)
-#SBATCH -o hostname_%j.out                 # File to which STDOUT will be written, including job ID (%j)
-#SBATCH -e hostname_%j.err                 # File to which STDERR will be written, including job ID (%j)
+#SBATCH -o jobs/$USER_%j.out                 # File to which STDOUT will be written, including job ID (%j)
+#SBATCH -e jobs/$USER_%j.err                 # File to which STDERR will be written, including job ID (%j)
 
 module load gcc/6.2.0 cuda/10.2 miniconda3/4.10.3 python/3.6.0
 
