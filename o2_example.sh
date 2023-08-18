808ca90403d35bee7a9589ca6792adc84d6a35dc
alexanderbates <alexander.shakeel.bates@gmail.com>
2023-08-18 00:00:29 -0400

    Update o2_example.sh

--- o2_example.sh
+++ o2_example.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#SBATCH -c 1                               # Request one core
+#SBATCH -c 4                               # Request cores
 #SBATCH -p gpu_quad                        # Use a quad GPU
 #SBATCH --gres=gpu:1                       # Number to use
 #SBATCH -t 2-00:00                         # Runtime in D-HH:MM format
-#SBATCH -p medium                          # Partition to run in
 #SBATCH --mem=50GB                         # Memory total in MiB (for all cores)
 #SBATCH -o hostname_%j.out                 # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e hostname_%j.err                 # File to which STDERR will be written, including job ID (%j)
